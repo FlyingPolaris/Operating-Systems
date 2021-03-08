a3d3b3f67b92bbf53d3e128822a1053a4f37d24f
Beichen Yu <polarisybc@qq.com>
2021-03-08 21:39:15 +0800

    Try jiffies

--- Project/Project1/simple/simple.c
+++ Project/Project1/simple/simple.c
@@ -14,12 +14,16 @@
 #include <linux/kernel.h>
 #include <linux/hash.h>
 #include <linux/gcd.h>
+#include <linux/jiffies.h>
 
 /* This function is called when the module is loaded. */
 int simple_init(void)
 {
        printk(KERN_INFO "Loading Kernel Module\n");
-       printk(KERN_INFO "%lu\n", GOLDEN_RATIO_PRIME);
+       printk(KERN_INFO "The Golden ratio prime is equal to %lu\n", GOLDEN_RATIO_PRIME);
+       printk(KERN_INFO "The value of jiffies is %lu\n", jiffies);
+       printk(KERN_INFO "The value of HZ is %lu\n", HZ);
+
 
        return 0;
 }
@@ -29,6 +33,8 @@ void simple_exit(void)
 {
        printk(KERN_INFO "Removing Kernel Module\n");
        printk(KERN_INFO "The gcd of 3300 and 24 is %lu\n", gcd(3300, 24));
+       printk(KERN_INFO "The value of jiffies is %lu\n", jiffies);
+
 }
 
 /* Macros for registering module entry and exit points. */

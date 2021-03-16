80bc8714fc47512671e35c8a47c92bf584bdd735
Beichen Yu <polarisybc@qq.com>
2021-03-16 22:43:24 +0800

    write some of redirect part

--- Project/Project2/simple-shell/simple-shell.c
+++ Project/Project2/simple-shell/simple-shell.c
@@ -15,12 +15,16 @@ void get_into_history(char **, char **, int, int);
 
 int main(void)
 {
+	pid_t pid;
+	int parent_wait = 1;
 	int history_exist = 0;
 	int num_of_args;
 	int history_num_of_args;
 	char *args[MAX_LINE / 2 + 1]; /* command line (of 80) has max of 40 arguments */
 	char history_args[MAX_LINE / 2 + 1];
 	int should_run = 1;
+	int input_red = 0, output_red = 0;
+	char input_file[MAX_LINE], output_file[MAX_LINE];
 	char buffer[MAX_LINE];
 
 	while (should_run)
@@ -29,15 +33,14 @@ int main(void)
 		fflush(stdout);
 
 		memset(buffer, 0, sizeof(buffer));
-
+		input_red = output_red = 0;
 		num_of_args = 0;
 		fgets(buffer, MAX_LINE, stdin);
 		char *token;
 		char delim[] = " \n\t";
 		for (token = strtok(buffer, delim); token != NULL; token = strtok(NULL, delim))
 		{
-			memset(args, 0, sizeof(token));
-			strcpy(args[num_of_args], token);
+			args[num_of_args] = token;
 			num_of_args++;
 		}
 
@@ -64,11 +67,40 @@ int main(void)
 			}
 		}
 
-		
 		if (strcmp(args[num_of_args - 1], "&") == 0)
 		{
+			parent_wait = 0;
+			memset(args[num_of_args - 1], 0, sizeof(args[num_of_args - 1]));
+			num_of_args--;
 		}
 
+		for (int i = 0; i < num_of_args; ++i)
+		{
+			if (strcmp(args[i], "<") == 0)
+			{
+				input_red = 1;
+				strcpy(input_file, args[i + 1]);
+			}
+			if (strcmp(args[i], ">") == 0)
+			{
+				output_red = 1;
+				strcpy(output_file, args[i + 1]);
+			}
+		}
+
+		pid = fork();
+		if (pid < 0)
+		{
+			printf("Fail to fork.\n");
+			return -1;
+		}
+		else if (pid == 0)
+		{
+			
+		}
+		else
+		{
+		}
 		/**
          	 * After reading user input, the steps are:
          	 * (1) fork a child process

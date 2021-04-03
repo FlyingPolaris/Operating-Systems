f3c201a4825c195bacd7b5c0d5d1136a488f1e70
FlyingPolaris <polarisybc@qq.com>
2021-04-03 21:44:42 +0800

    write something basic

--- Project/Project5/Designing-a-Thread-Pool/threadpool.c
+++ Project/Project5/Designing-a-Thread-Pool/threadpool.c
@@ -5,6 +5,7 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <semaphore.h>
 #include "threadpool.h"
 
@@ -13,31 +14,43 @@
 
 #define TRUE 1
 
-// this represents work that has to be 
+// this represents work that has to be
 // completed by a thread in the pool
-typedef struct 
+typedef struct
 {
     void (*function)(void *p);
     void *data;
-}
-task;
+} task;
 
 // the work queue
+task workqueue[QUEUE_SIZE];
 task worktodo;
+pthread_mutex_t mutex;
+sem_t full;
+sem_t empty;
 
 // the worker bee
-pthread_t bee;
+pthread_t bee[NUMBER_OF_THREADS];
+int rear, front;
 
 // insert a task into the queue
-// returns 0 if successful or 1 otherwise, 
-int enqueue(task t) 
+// returns 0 if successful or 1 otherwise,
+int enqueue(task t)
 {
+    pthread_mutex_lock(&mutex);
+    workqueue[rear] = t;
+    rear = (rear + 1) % QUEUE_SIZE;
+    pthread_mutex_unlock(&mutex);
     return 0;
 }
 
 // remove a task from the queue
-task dequeue() 
+task dequeue()
 {
+    pthread_mutex_lock(&mutex);
+    worktodo = workqueue[front];
+    front = (front + 1) % NUMBER_OF_THREADS;
+    pthread_mutex_unlock(&mutex);
     return worktodo;
 }
 
@@ -46,7 +59,7 @@ void *worker(void *param)
 {
     // execute the task
     execute(worktodo.function, worktodo.data);
-
+    
     pthread_exit(0);
 }
 
@@ -72,11 +85,18 @@ int pool_submit(void (*somefunction)(void *p), void *p)
 // initialize the thread pool
 void pool_init(void)
 {
-    pthread_create(&bee,NULL,worker,NULL);
+    rear = front = 0;
+    sem_init(&full, 0, QUEUE_SIZE);
+    sem_init(&empty, 0, 0);
+    memset(workqueue, 0, sizeof(workqueue));
+    for (int i = 0; i < NUMBER_OF_THREADS; ++i)
+    {
+        pthread_create(&bee[i], NULL, worker, NULL);
+    }
 }
 
 // shutdown the thread pool
 void pool_shutdown(void)
 {
-    pthread_join(bee,NULL);
+    pthread_join(bee, NULL);
 }
